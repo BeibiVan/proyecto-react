66581e682d37934269fc588feff1df7eb9775b46
Ivan Dziuba <ivaan.id@gmail.com>
2022-05-17 17:05:21 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav class="navbar navbar-expand-lg color-nav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">↓LOGO↑</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <button class="btn color-search" type="submit">Search</button>
+      </form>
+      </div>
+  </div>
+      </nav>
       </header>
+      <footer><h4>Creado por Ivan Dziuba</h4></footer>
     </div>
+   
   );
 }
 
